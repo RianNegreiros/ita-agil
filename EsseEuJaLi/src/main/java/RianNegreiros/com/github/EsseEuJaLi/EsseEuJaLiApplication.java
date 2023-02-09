a0b7a686fb06452fe5431f54723fc7aa9db8a2e6
Rian Negreiros <riannegreiros@gmail.com>
2023-02-09 15:48:47 -0300

    add spring

---
+++ EsseEuJaLi/src/main/java/RianNegreiros/com/github/EsseEuJaLi/EsseEuJaLiApplication.java
@@ -0,0 +1,13 @@
+package RianNegreiros.com.github.EsseEuJaLi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsseEuJaLiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsseEuJaLiApplication.class, args);
+	}
+
+}
---
+++ EsseEuJaLi/src/test/java/RianNegreiros/com/github/EsseEuJaLi/EsseEuJaLiApplicationTests.java
@@ -0,0 +1,13 @@
+package RianNegreiros.com.github.EsseEuJaLi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsseEuJaLiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
